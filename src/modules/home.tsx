6fa2737bdd06f190ee3d70c8adcdea11e87f4ddc
IzzatRushaidhi <muhammadizzatzuhairi@ifca.com.my>
2022-08-30 00:39:59 +0800

    finish up

--- src/modules/home.tsx
+++ src/modules/home.tsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./home.css";
 import sun from "../assets/sun.png";
 import pi from "../assets/pi.png";
 import { Button } from "@mui/material";
+import axios from "axios";
+import BigNumber from "bignumber.js";
 
 export const Home = () => {
-  const radiusOfTheSun = "696,340";
-  const piVal = "3.1415";
-  const cirSun = "2,000,000";
+  const [piVal, setPiVal] = useState<BigNumber>(new BigNumber(0));
+  const radiusOfTheSun = "696340";
+
+  useEffect(() => {
+    if (piVal.toNumber() === 0) {
+      axios.get("http://localhost:4500/api/calc-pi-async").then((val) => {
+        setPiVal(val?.data);
+      });
+    }
+  }, []);
+
+  const calcSunCir = (rad: string) => {
+    const radius = new BigNumber(rad);
+    const cirSun = radius.times(piVal).times(2);
+    return cirSun.toFormat(4);
+  };
 
   return (
     <div className="homepage">
@@ -24,27 +39,54 @@ export const Home = () => {
               <h3 className="h3-top">~</h3>
             </div>
             <div>
-              <h3 className="h3-top">{piVal}</h3>
+              <h3 className="h3-top">{piVal.toString()}</h3>
             </div>
           </div>
           <div className="pi-img-container">
             <div className="two-pi-r">
               <div>r</div>
-              <div className="cir-sun"> ~ {radiusOfTheSun} km</div>
+              <div className="cir-sun">
+                {" "}
+                ~ {BigNumber(radiusOfTheSun).toFormat()} km
+              </div>
             </div>
           </div>
           <div className="pi-img-container">
             <div className="two-pi-r">
               <div>2πr</div>
-              <div className="cir-sun"> ~ {cirSun} km</div>
+              <div className="cir-sun"> ~ {calcSunCir(radiusOfTheSun)} km</div>
             </div>
           </div>
         </div>
         <div className="footer-btn">
-          <Button className="button" variant="contained">
+          <Button
+            className="button"
+            variant="contained"
+            onClick={() => {
+              axios.get("http://localhost:4500/api/reset-pi").then((val) => {
+                if (val.data) {
+                  axios
+                    .get("http://localhost:4500/api/calc-pi-async")
+                    .then((val) => {
+                      setPiVal(val?.data);
+                    });
+                }
+              });
+            }}
+          >
             Reset pi value
           </Button>
-          <Button className="button" variant="contained">
+          <Button
+            className="button"
+            variant="contained"
+            onClick={() => {
+              axios
+                .get("http://localhost:4500/api/calc-pi-async")
+                .then((val) => {
+                  setPiVal(val?.data);
+                });
+            }}
+          >
             Calculate pi
           </Button>
         </div>
